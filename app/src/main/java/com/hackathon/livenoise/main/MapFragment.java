d63339ab76b354976fa6bbbcb259f0ac939ce2b9
ThanhSonITNIC <ThanhSon.ITNIC@gmail.com>
2019-10-09 17:49:24 +0700

    Initial commit

---
+++ app/src/main/java/com/hackathon/livenoise/main/MapFragment.java
@@ -0,0 +1,64 @@
+package com.hackathon.livenoise.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.hackathon.livenoise.R;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapViewModel mViewModel;
+
+    private GoogleMap mMap;
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+        // TODO: Use the ViewModel
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+}
---
+++ app/src/main/java/com/hackathon/livenoise/main/MapViewModel.java
@@ -0,0 +1,8 @@
+package com.hackathon.livenoise.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+}
